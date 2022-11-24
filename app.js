5e8413c76381056d581260135c87a47ea89f61ca
Y o u s s e f - E l s i s y <110091301+usefsame7@users.noreply.github.com>
2022-11-24 04:16:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ require("./db/connect");
 const app = express();
 const bookRoutes = require('./routs/bookRoutes')
 
-// Set Template Engine
+// Template Engine
 app.set("view engine", "ejs");
 
 
 port = 3000;
-
 app.listen(port, () => console.log(`App is listening on port ${port} ...`));
+
+     // connecting ot database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true ,useUnifiedTopology: true })
+   .then(() => console.log("Database connected ..."))
+    .catch(err => console.log( err.message ));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 
 
-
-
-
-
 app.use('/', bookRoutes);
+
